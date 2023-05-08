b0368a32a0af1194e14dbc7e759cc9c173802c93
springyu <dofnstudy10212@gmail.com>
2023-05-08 19:18:21 +0800

    鍵盤操控

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,49 @@
 //Using SDL and standard IO
 #include <SDL2/SDL.h>
-#include <cstdio>
+#include <iostream>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main( int argc, char* args[] ){
-    //The window we'll be rendering to
+
     SDL_Window* window = NULL;
-    
-    //The surface contained by the window
     SDL_Surface* screenSurface = NULL;
 
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    }
-    else{
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL ){
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        }
-        else{
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
+    SDL_Init( SDL_INIT_VIDEO );
+    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    
+    bool isRunning = true;
 
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
+    SDL_Event e;
 
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+    while(isRunning){
+
+        while(SDL_PollEvent(&e) != 0){
+            if(e.type == SDL_QUIT){
+                isRunning = false;
+            }
+
+            if(e.type == SDL_KEYDOWN){
+
+                if(e.key.keysym.sym == SDLK_1){
+                    std::cout << 1 << '\n';
+                }
+                else if(e.key.keysym.sym == SDLK_2){
+                    std::cout << 2 << '\n';
+                }
+                else if (e.key.keysym.sym == SDLK_3){
+                    std::cout << 3 << '\n';
+                }
+
+            }
         }
+        SDL_UpdateWindowSurface( window );
     }
-        //Destroy window
+
+    
+
     SDL_DestroyWindow( window );
 
     //Quit SDL subsystems
