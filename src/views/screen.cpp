8dd5501667dd0e5f664c043e8dd3ccab9b27f612
springyu <dofnstudy10212@gmail.com>
2023-05-31 19:10:39 +0800

    screen edit

--- src/views/screen.cpp
+++ src/views/screen.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdio>
 #include "screen.h"
 
 app::app(){
-
+    quit = false;
 }
 
 app::~app(){
@@ -22,7 +24,7 @@ void app::init(){
     window = SDL_CreateWindow("My SDL Empty window", 
                                 SDL_WINDOWPOS_UNDEFINED, 
                                 SDL_WINDOWPOS_UNDEFINED, 
-                                640, 480, 0);
+                                640, 480, SDL_WINDOW_SHOWN);
 
     renderer = SDL_CreateRenderer(window, -1, 0);
     image = SDL_LoadBMP("imgs/green.bmp");
@@ -32,6 +34,27 @@ void app::init(){
     }
     texture = SDL_CreateTextureFromSurface(renderer, image);
 
+}
+
+void app::load_picture(){
+}
+
+void app::render(){
+}
+
+void app::create_map(){
+
+    std::string s[10];
+    std::ifstream file("./maps/map.txt", std::ios::in);
+    int cnt = 0;
+    int x, y;
+    file >> x >> y;
+    while(!file.eof()){
+        file >> s[cnt];
+        std::cout << s[cnt] << '\n';
+        cnt++;
+    }
+    std::cout << x << " " << y << '\n';
 
 }
 
--- src/views/screen.h
+++ src/views/screen.h
@@ -6,20 +6,25 @@
 class app{
 
     private:
-        bool quit = false;
+        bool quit;
         SDL_Event event;
         SDL_Window *window;
         SDL_Renderer *renderer;
         SDL_Surface *image;
         SDL_Texture *texture;
-        
+
 
     public:
         app();
         ~app();
 
+        void load_picture();
+        void render();
+        void screen();
+        
         void init();
-
+        
+        void create_map();
         void run();
 
 
